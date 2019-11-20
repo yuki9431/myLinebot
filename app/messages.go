2a49e56878fea79ad9f8ba1e848e5b6ad574b104
yuki9431 <yuki9431@gmail.com>
2019-11-21 07:12:41 +0900

    add メッセージ判定用の関数追加

---
+++ app/messages.go
@@ -0,0 +1,21 @@
+package main
+
+// IsOjichan おじいさんの呼びかけに反応する
+func IsOjichan() (isOjichan bool) {
+	return true
+}
+
+// IsAskWeather 天気を尋ねられたとき
+func IsAskWeather() (isAskWeather bool) {
+	return true
+}
+
+// IsChangeCity 所在地を変更するとき
+func IsChangeCity() (isChangeCity bool) {
+	return true
+}
+
+// IsShowCityList 都市一覧を表示するとき
+func IsShowCityList() (isShowCityList bool) {
+	return true
+}
