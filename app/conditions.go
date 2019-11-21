599c7f948ba2a1ce2e29af28d2a0b795299d0fa9
yuki9431 <yuki9431@gmail.com>
2019-11-22 06:00:01 +0900

    mod メッセージ判定用の関数の処理を記載

--- app/conditions.go
+++ app/conditions.go
@@ -1,21 +1,80 @@
 package main
 
+import (
+	"strings"
+)
+
 // IsOjichan おじいさんの呼びかけに反応する
-func IsOjichan() (isOjichan bool) {
-	return true
+func IsOjichan(m string) (isOjichan bool) {
+	messages := []string{
+		"おじいさん",
+		"オジイサン",
+		"おじいちゃん",
+		"オジイチャン",
+		"おじさん",
+		"オジサン",
+		"おじちゃん",
+		"オジチャン",
+		"じじい",
+		"ジジイ",
+	}
+
+	return contains(m, messages)
 }
 
 // IsAskWeather 天気を尋ねられたとき
-func IsAskWeather() (isAskWeather bool) {
-	return true
+func IsAskWeather(m string) (isAskWeather bool) {
+	messages := []string{
+		"天気",
+		"てんき",
+		"テンキ",
+		"暑い",
+		"寒い",
+	}
+
+	return contains(m, messages)
 }
 
 // IsChangeCity 所在地を変更するとき
-func IsChangeCity() (isChangeCity bool) {
-	return true
+func IsChangeCity(m string) (isChangeCity bool) {
+	messages := []string{
+		"都市変更:",
+	}
+
+	return contains(m, messages)
 }
 
 // IsShowCityList 都市一覧を表示するとき
-func IsShowCityList() (isShowCityList bool) {
-	return true
+func IsShowCityList(m string) (isShowCityList bool) {
+	messages := []string{
+		"都市一覧",
+	}
+
+	return contains(m, messages)
+}
+
+// IsShowHelp 都市一覧を表示するとき
+func IsShowHelp(m string) (isShowCityList bool) {
+	messages := []string{
+		"へるぷ",
+		"ヘルプ",
+		"助けて",
+		"たすけて",
+		"Help",
+		"help",
+	}
+
+	return contains(m, messages)
+}
+
+// Contains 文字列リストと引数の文字列が一致するか判別する
+func contains(s string, strs []string) (isContainable bool) {
+	isContainable = false
+
+	for _, str := range strs {
+		if strings.Contains(s, str) {
+			isContainable = true
+		}
+	}
+	return isContainable
 }
