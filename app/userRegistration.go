82382a7a804d5da4f84e8f6fc7d766f392b14165
yuki9431 <yuki9431@gmail.com>
2019-11-22 06:05:04 +0900

    add ユーザ登録＆解除のモジュール追加

---
+++ app/userRegistration.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/yuki9431/mongohelper"
+)
+
+// Follow ユーザがフォローした際の処理
+func Follow(profile *linebot.UserProfileResponse) (replyMessage string, err error) {
+	mongo, err := mongohelper.NewMongo(mongoDial, mongoName)
+	if err != nil {
+		return
+	}
+
+	userInfo := new(UserInfo)
+	userInfo.UserID = profile.UserID
+	userInfo.DisplayName = profile.DisplayName
+	userInfo.CityID, _ = GetCityID("東京") //初回登録時には問答無用で東京民や
+	userInfo.PictureURL = profile.PictureURL
+	userInfo.StatusMessage = profile.StatusMessage
+
+	// ユーザ情報をDBに登録
+	mongo.InsertDb(userInfo, "userInfos")
+
+	// フレンド登録時の挨拶
+	var replyMessages [5]string
+	replyMessages[0] = profile.DisplayName + "さん\nはじめまして、毎朝6時に天気情報を教えてあげるね"
+	replyMessages[1] = usage
+	replyMessages[2] = "お住まいの都市を変更するには、下記の通りメッセージをお送りください"
+	replyMessages[3] = "都市変更:東京"
+	replyMessages[4] = "都市変更:Brasil"
+
+	for _, replyMessage = range replyMessages {
+		replyMessage += replyMessage
+	}
+
+	return
+}
+
+// UnFollow ユーザがブロックした際の処理
+func UnFollow(profile *linebot.UserProfileResponse) (replyMessage string, err error) {
+	return "", nil
+}
